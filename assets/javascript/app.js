e168c55b288f83d7fe7679daa83b99a0bcc71481
Joe <dinhx068@umn.edu>
2018-08-07 02:24:33 -0500

    some more progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,67 +1,106 @@
 $(document).ready(function(){
-  $("#start-button").on('click', trivia.start);
+  $("#start-button").on("click", game.start);
+  $(".choice-button").on("click", game.checkGuess);
 })
   
-  var trivia = {
+  var game = {
     // Using caps for global variables
     CORRECT: 0,
     INCORRECT: 0,
-    CURRENTQUESTION: 0,
+    DIDNOTANSWER: 0,
+    QUESTIONNUMBER: 0,
     TIMER: 10,
     TIMERFLAG: false,
-    TIMERID : '',
+    TIMERID : "",
 
-    questions: {
-      q1: 'We can find Captain Salazar in which movie?',
-      q2: "In Harry Potter, what is Dumbledore's first name?",
-      q3: 'Abominable Snowman is from which Disney movie?',
-      q4: 'Which of these Alien movies is set at a foundry facility and penal colony?',
-      q5: "What is the fifth Star Wars movie called?",
-      q6: 'In the Hunger Games, what district is Thresh from?',
-      q7: "In Frozen, how many sisters does Elsa have?",
-      q8: "Who does the voice of 'Marty' in Madagascar?",
-      q9: "How many of the Ryan brothers died in WWII ?",
-      q10: "What actor was offered the role of Maximus, but turned it down?"
+    question: {
+      n1: "We can find Captain Salazar in which movie?",
+      n2: 'In Harry Potter, what is Dumbledore"s first name?',
+      n3: "Abominable Snowman is from which Disney movie?",
+      n4: "Which of these Alien movies is set at a foundry facility and penal colony?",
+      n5: "What is the fifth Star Wars movie called?",
+      n6: "In the Hunger Games, what district is Thresh from?",
+      n7: "In Frozen, how many sisters does Elsa have?",
+      n8: 'Who does the voice of "Marty" in Madagascar?',
+      n9: "How many of the Ryan brothers died in WWII ?",
+      n10: "What actor was offered the role of Maximus, but turned it down?"
     },
     choices: {
-      q1: ['Justice League', 'The Greatest Showman', 'Pirates of the Carribbean', 'American Made'],
-      q2: ['Albus', 'Severus', 'Voldemort', 'Arthur'],
-      q3: ['Hercules', 'Monsters,Inc.', 'Aladdin', 'Tarzan'],
-      q4: ['Aliens', 'Aliens:Resurrection', 'Alien 3', 'Alien'],
-      q5: ['A New Hope','Return of the Jedi','The Empire Strikes Back','Revenge of the Sith'],
-      q6: ['4','7','9','11'],
-      q7: ['1','2','3','4'],
-      q8: ['Chris Rock','David Schwimmer','Matthew Broderick','Chris Tucker'],
-      q9: ['1','2','3','4'],
-      q10: ['George Clooney','Mel Gibson','Bruce Willis','Tom Selleck']
+      n1: ["Justice League", "The Greatest Showman", "Pirates of the Carribbean", "American Made"],
+      n2: ["Albus", "Severus", "Voldemort", "Arthur"],
+      n3: ["Hercules", "Monsters,Inc.", "Aladdin", "Tarzan"],
+      n4: ["Aliens", "Aliens:Resurrection", "Alien 3", "Alien"],
+      n5: ["A New Hope","Return of the Jedi","The Empire Strikes Back","Revenge of the Sith"],
+      n6: ["4","7","9","11"],
+      n7: ["1","2","3","4"],
+      n8: ["Chris Rock","David Schwimmer","Matthew Broderick","Chris Tucker"],
+      n9: ["1","2","3","4"],
+      n10: ["George Clooney","Mel Gibson","Bruce Willis","Tom Selleck"]
     },
-    answers: {
-      q1: "Pirates of the Carribbean",
-      q2: "Albus",
-      q3: "Monsters,Inc.",
-      q4: "Alien 3",
-      q5: "The Empire Strikes Back",
-      q6: "11",
-      q7: "1",
-      q8: "Chris Rock",
-      q9: "3",
-      q10: "Mel Gibson"
+    answer: {
+      n1: "Pirates of the Carribbean",
+      n2: "Albus",
+      n3: "Monsters,Inc.",
+      n4: "Alien 3",
+      n5: "The Empire Strikes Back",
+      n6: "11",
+      n7: "1",
+      n8: "Chris Rock",
+      n9: "3",
+      n10: "Mel Gibson"
     },
 
     // FUNCTIONS ====================================================================================================================================
     start: function() {
       // If the player decides to click the Start Game button then the game resetting the variables below
-      trivia.CURRENTQUESTION = 0;
-      trivia.CORRECT = 0;
-      trivia.INCORRECT = 0;
-      trivia.UNANSWERED = 0;
-      clearInterval(trivia.TIMERID);
+      game.CURRENTQUESTION = 0;
+      game.CORRECT = 0;
+      game.INCORRECT = 0;
+      game.DIDNOTANSWER = 0;
+      clearInterval(game.TIMERID);
 
-      $("#results").html("");
-      $("#time-remaining").text(trivia.TIMER);
+      $("#h1").text("Game in progress. . .");
+      $("#h2").text("Choose your answer");
       $("#start-button").hide();
-      $("#h2").hide();
+      $("#results").html("");
       $("#game").show();
+      document.getElementById("time").style.visibility = "visible";
       $("#remaining-time").show();
+      $("#time-remaining").text(game.TIMER);
+
+      game.selectQuestion();
     },
+
+    selectQuestion: function() {
+      game.TIMER = 15;
+      $("#time-remaining").text(game.TIMER);
+
+      if(!game.TIMERFLAG){
+        game.TIMERID = setInterval(game.timeLeft, 1000);
+      }
+
+      var question = Object.values(game.question)[game.QUESTIONNUMBER];
+      console.log(question);
+      $("#display-question").text(question);
+      
+      var choices = Object.values(game.choices)[game.QUESTIONNUMBER];
+      console.log(choices);
+
+      $.each(choices, function(index, key){
+        $("#choices-to-choose-from").append($('<button class="choice-button btn btn-default btn-lg btn-block">' + key + '</button>'));
+      })
+    },
+
+    timeLeft: function() {
+      if(game.TIMER > -1 && game.QUESTIONNUMBER < Object.keys(game.question).length){
+        $("#time-remaining").text(game.TIMER);
+        console.log(game.TIMER);
+        game.TIMER--;
+      }
+    },
+
+    checkGuess: function(){
+      console.log("testing guess");
+    },
+    
   }
