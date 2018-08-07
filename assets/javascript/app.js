15b1a2604f28345d751a9d159f94daf804d3c735
Joe <dinhx068@umn.edu>
2018-08-06 19:39:10 -0500

    some more stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
-
+  $("#start-button").on('click', trivia.start);
 })
   
   var trivia = {
-    correct: 0,
-    incorrect: 0,
-    currentQuestion: 0,
-    timer: 10,
-    timerOn: false,
-    timerId : '',
+    // Using caps for global variables
+    CORRECT: 0,
+    INCORRECT: 0,
+    CURRENTQUESTION: 0,
+    TIMER: 10,
+    TIMERFLAG: false,
+    TIMERID : '',
 
     questions: {
       q1: 'We can find Captain Salazar in which movie?',
@@ -22,7 +23,7 @@ $(document).ready(function(){
       q9: "How many of the Ryan brothers died in WWII ?",
       q10: "What actor was offered the role of Maximus, but turned it down?"
     },
-    options: {
+    choices: {
       q1: ['Justice League', 'The Greatest Showman', 'Pirates of the Carribbean', 'American Made'],
       q2: ['Albus', 'Severus', 'Voldemort', 'Arthur'],
       q3: ['Hercules', 'Monsters,Inc.', 'Aladdin', 'Tarzan'],
@@ -35,15 +36,32 @@ $(document).ready(function(){
       q10: ['George Clooney','Mel Gibson','Bruce Willis','Tom Selleck']
     },
     answers: {
-      q1: 'Pirates of the Carribbean',
-      q2: 'Albus',
-      q3: 'Monsters,Inc.',
-      q4: 'Alien 3',
-      q5: 'The Empire Strikes Back',
-      q6: '11',
-      q7: '1',
-      q8: 'Chris Rock',
-      q9: '3',
-      q10: 'Mel Gibson'
+      q1: "Pirates of the Carribbean",
+      q2: "Albus",
+      q3: "Monsters,Inc.",
+      q4: "Alien 3",
+      q5: "The Empire Strikes Back",
+      q6: "11",
+      q7: "1",
+      q8: "Chris Rock",
+      q9: "3",
+      q10: "Mel Gibson"
+    },
+
+    // FUNCTIONS ====================================================================================================================================
+    start: function() {
+      // If the player decides to click the Start Game button then the game resetting the variables below
+      trivia.CURRENTQUESTION = 0;
+      trivia.CORRECT = 0;
+      trivia.INCORRECT = 0;
+      trivia.UNANSWERED = 0;
+      clearInterval(trivia.TIMERID);
+
+      $("#results").html("");
+      $("#time-remaining").text(trivia.TIMER);
+      $("#start-button").hide();
+      $("#h2").hide();
+      $("#game").show();
+      $("#remaining-time").show();
     },
   }
